ac887bd6e755b61d25d9bf3bd0857ea1f375d6d8
Valeriu Olariu <ovaleriu7@gmail.com>
2019-04-10 09:17:09 +0100

    Instrument details index

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -7,7 +7,7 @@ class Instrument < ApplicationRecord
     validates :description, length: {maximum: 1000, too_long: "%{count} characters is the maximum alowed. "}
 
     validates :title, length: {maximum: 140, too_long: "%{count} Maximum caracter aloud."}
-    validates :price, numericality: {only_integer: true}, length: {maximum: 7 }
+    validates :price, length: {maximum: 7 }
 
     BRAND = %w{ Fender Gibson Epiphone ESP Martin Dean Taylor Jackson PRS  Ibanez Charvel Washburn }
     FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
